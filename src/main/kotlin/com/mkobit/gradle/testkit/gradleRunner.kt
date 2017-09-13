71902e71ba16845986fb5170376f3a0f0babdf57
Mike Kobit <mkobit@gmail.com>
2017-09-13 07:17:54 -0500

    Add extension method to resolve a `Path` relative to the `projectDir` for GradleRunner
    This can be useful when running assertions on files output by a build.

--- src/main/kotlin/com/mkobit/gradle/testkit/gradleRunner.kt
+++ src/main/kotlin/com/mkobit/gradle/testkit/gradleRunner.kt
@@ -3,6 +3,7 @@ package com.mkobit.gradle.testkit
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import java.io.File
+import java.nio.file.Path
 
 fun build(projectDirectory: File, vararg arguments: String): BuildResult {
   return GradleRunner.create()
@@ -18,4 +19,15 @@ fun buildAndFail(projectDirectory: File, vararg arguments: String): BuildResult 
     .withProjectDir(projectDirectory)
     .withArguments(*arguments)
     .buildAndFail()
+}
+
+/**
+ * Get the path resolved against the [GradleRunner.getProjectDir] path.
+ * @throws IllegalStateException when [GradleRunner.getProjectDir] is `null`
+ * @throws IllegalArgumentException when [other] is an absolute path
+ */
+fun GradleRunner.resolve(other: Path): Path {
+  require(!other.isAbsolute) { "Path $other must not be absolute" }
+  val projectDirectory: File = projectDir ?: throw IllegalStateException("projectDir must not be null to resolve path")
+  return projectDirectory.toPath().resolve(other)
 }
