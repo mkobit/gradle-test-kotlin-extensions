60dc7dfac7dea656ae210cabbd617ce4a9fe367d
Mike Kobit <mkobit@gmail.com>
2017-11-13 15:53:33 -0600

    Merge two JUnit type `testpupport` files

--- src/test/kotlin/testsupport/junit.kt
+++ src/test/kotlin/testsupport/junit.kt
@@ -2,6 +2,22 @@ package testsupport
 
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+class BooleanArgumentsProvider : ArgumentsProvider {
+  override fun provideArguments(context: ExtensionContext): Stream<out Arguments> = Stream.of(
+      Arguments.of(true),
+      Arguments.of(false)
+  )
+}
+
+@ArgumentsSource(BooleanArgumentsProvider::class)
+annotation class BooleanSource
+
 
 /**
  * Creates a [DynamicTest.dynamicTest] with the provided [displayName] and provides a new [GradleRunner] instance
