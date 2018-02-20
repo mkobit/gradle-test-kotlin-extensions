4d782c265134da807ca1d9b6af1319d5bd031f1f
Mike Kobit <mkobit@gmail.com>
2018-02-19 20:22:05 -0600

    Disable another test on Java 9 CI

--- src/test/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/NullRunnerConfigurerTest.kt
+++ src/test/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/NullRunnerConfigurerTest.kt
@@ -5,8 +5,10 @@ import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 
 internal class NullRunnerConfigurerTest {
+  @DisabledIfEnvironmentVariable(named = "CIRCLE_JOB", matches = "^test-java-9$")
   @Test
   internal fun `does not mutate the GradleRunner`() {
     val mockRunner: GradleRunner = mock()
