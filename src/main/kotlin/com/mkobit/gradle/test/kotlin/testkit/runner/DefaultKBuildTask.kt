6826663950e553f83e4ad56d2f9acb85fe13cbf0
Mike Kobit <mkobit@gmail.com>
2018-04-11 11:11:53 -0500

    Add `toString()` implementation for default `KBuildTask` and `KBuildResult` implementations
    closes https://github.com/mkobit/gradle-test-kotlin-extensions/issues/32

--- src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/DefaultKBuildTask.kt
+++ src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/DefaultKBuildTask.kt
@@ -2,4 +2,10 @@ package com.mkobit.gradle.test.kotlin.testkit.runner
 
 import org.gradle.testkit.runner.BuildTask
 
-internal class DefaultKBuildTask(private val delegate: BuildTask) : KBuildTask, BuildTask by delegate
+/**
+ * Default implementation of [KBuildTask].
+ * @property delegate the actual build task
+ */
+internal class DefaultKBuildTask(private val delegate: BuildTask) : KBuildTask, BuildTask by delegate {
+  override fun toString(): String = "DefaultKBuildTask(path=${delegate.path}, outcome=${delegate.outcome})"
+}
