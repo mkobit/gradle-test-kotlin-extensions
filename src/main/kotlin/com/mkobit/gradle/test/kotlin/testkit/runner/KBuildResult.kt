d56b857078a85cccb4e1165f829fb2a23e00c2a1
Mike Kobit <mkobit@gmail.com>
2018-04-11 11:31:29 -0500

    Add KDoc to `KBuildResult`

--- src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/KBuildResult.kt
+++ src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/KBuildResult.kt
@@ -8,12 +8,26 @@ import java.nio.file.Path
  * An extension of a [BuildResult].
  */
 interface KBuildResult : BuildResult {
+  /**
+   * Retrieves the task at the given path.
+   * @param taskPath the path of the task, for example `":build"`.
+   */
   override fun task(taskPath: String): KBuildTask?
 
+  /**
+   * Retrieves the tasks that have the provided [TaskOutcome]
+   */
   override fun tasks(outcome: TaskOutcome): List<KBuildTask>
 
+  /**
+   * Retrieves all tasks.
+   */
   override fun getTasks(): List<KBuildTask>
 
+  /**
+   * Retries all task paths with the provided [TaskOutcome].
+   * @param outcome the outcome to filter by
+   */
   override fun taskPaths(outcome: TaskOutcome): List<String>
 
   /**
