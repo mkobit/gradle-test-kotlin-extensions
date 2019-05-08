55b8f95ef81c68dee75cd9a46cb0bc753f7f286c
Mike Kobit <mkobit@gmail.com>
2019-05-08 09:42:50 -0500

    Deprecate `GradleRunner.projectDirPath`

--- src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/fs.kt
+++ src/main/kotlin/com/mkobit/gradle/test/kotlin/testkit/runner/fs.kt
@@ -9,13 +9,14 @@ import java.nio.file.Path
  * @param action the action to apply to the [FileContext.DirectoryContext] instance
  */
 fun GradleRunner.setupProjectDir(action: FileContext.DirectoryContext.() -> Unit): GradleRunner = apply {
-  val path = projectDirPath ?: throw IllegalStateException("project directory must be specified")
+  val path = projectDir?.toPath() ?: throw IllegalStateException("project directory must be specified")
   FileContext.DirectoryContext(path).run(action)
 }
 
 /**
  * The [GradleRunner.getProjectDir] as a [Path].
  */
+@Deprecated("Superfluous getter method, will be removed", replaceWith = ReplaceWith("projectDir?.toPath()"))
 var GradleRunner.projectDirPath: Path?
   get() = projectDir?.toPath()
   set(value) {
